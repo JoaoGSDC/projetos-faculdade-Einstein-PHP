c0a4172964cd4ac762b4219f8f35c53153f4afc5
João Gabriel <62730271+JoaoGSDC@users.noreply.github.com>
2020-04-24 00:20:57 -0300

    init

---
+++ prova2/index.php
@@ -0,0 +1,38 @@
+<html>
+
+<head>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="./styles.css">
+    <title>Medicorp</title>
+</head>
+
+<body>
+
+    <div class="navbar"><span>Medicorp</span></div>
+    <div style="height: 650px;">
+    <img src="./shared/image.jpg" alt="hospital" />
+    </div>
+
+    <div class="container-option">
+        <div class="container-icons">
+            <div class="container-icon">
+                <img class="icons" src="./shared/medico.svg" alt="hospital" />
+                <label class="icons-text">HOSPITAL</label>
+            </div>
+
+            <div class="container-icon">
+                <img class="icons" src="./shared/espirrar.svg" alt="hospital" />
+                <label class="icons-text">PATIENT</label>
+            </div>
+
+            <div class="container-icon">
+                <img class="icons" src="./shared/lista-de-controle.svg" alt="hospital" />
+                <label class="icons-text">LIST</label>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
