f499b21b8ff7d6e125984c287d558850a0cba974
JoaoGSDC <joaogsdc@gmail.com>
2020-04-28 22:03:11 -0300

    main

--- prova2/index.php
+++ prova2/index.php
@@ -2,6 +2,7 @@
 
 <head>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="./styles.css">
     <title>Medicorp</title>
 </head>
@@ -9,11 +10,19 @@
 <body>
 
     <div class="navbar"><span>Medicorp</span></div>
-    <div style="height: 650px;">
-    <img src="./shared/image.jpg" alt="hospital" />
+
+
+
+    <div style="height: 750px;">
+        <div class="container-text-img">
+            <h1 class="text-img">Seja muito bem vindo!</h1>
+            <h1 class="text-img logo">MEDICORP</h1>
+        </div>
+        <img class="img" src="./shared/image.jpg" alt="hospital" />
     </div>
 
     <div class="container-option">
+
         <div class="container-icons">
             <div class="container-icon">
                 <img class="icons" src="./shared/medico.svg" alt="hospital" />
@@ -31,6 +40,75 @@
             </div>
         </div>
 
+    </div>
+
+    <div class="card-table">
+        <h1>Casos de COVID-19</h1>
+
+        <div style="overflow-x:auto;">
+            <table>
+                <tr>
+                    <th>Local</th>
+                    <th>Confirmados</th>
+                    <th>Recuperados</th>
+                    <th>Mortes</th>
+                </tr>
+                <tr>
+                    <td>São Paulo</td>
+                    <td>24.041</td>
+                    <td>-</td>
+                    <td>2.049</td>
+                </tr>
+                <tr>
+                    <td>Rio de Janeiro</td>
+                    <td>8.504</td>
+                    <td>-</td>
+                    <td>738</td>
+                </tr>
+                <tr>
+                    <td>Ceará</td>
+                    <td>6.918</td>
+                    <td>-</td>
+                    <td>403</td>
+                </tr>
+                <tr>
+                    <td>Pernambuco</td>
+                    <td>5.724</td>
+                    <td>-</td>
+                    <td>508</td>
+                </tr>
+                <tr>
+                    <td>Amazonas</td>
+                    <td>4.337</td>
+                    <td>-</td>
+                    <td>351</td>
+                </tr>
+                <tr>
+                    <td>Bahia</td>
+                    <td>2.540</td>
+                    <td>-</td>
+                    <td>86</td>
+                </tr>
+                <tr>
+                    <td>Maranhão</td>
+                    <td>2.528</td>
+                    <td>-</td>
+                    <td>145</td>
+                </tr>
+                <tr>
+                    <td>Pará</td>
+                    <td>2.262</td>
+                    <td>-</td>
+                    <td>129</td>
+                </tr>
+                <tr>
+                    <td>Espírito Santo</td>
+                    <td>1.874</td>
+                    <td>-</td>
+                    <td>64</td>
+                </tr>
+            </table>
+        </div>
     </div>
 
 </body>
